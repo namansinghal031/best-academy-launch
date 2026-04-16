dec9b34c70b3c2e28d4b832ae1fa2fcfb19b4ae8
namansinghl031 <111794070+namansinghal031@users.noreply.github.com>
2026-04-16 23:41:01 +0530

    Add HashRouter to main.tsx for routing
    Wrap App component with HashRouter for routing support.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom";   // 👈 add this
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <HashRouter>   {/* 👈 wrap App */}
+    <App />
+  </HashRouter>
+);
